e97c6fd711a1194fa852d3f54b0bd19970d787db
zinglifet <zinglifet@sina.com>
2019-12-27 18:13:11 +0800

    Initial commit

---
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -0,0 +1,43 @@
+package life.majiang.community.controller;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import life.majiang.community.dto.PaginationDTO;
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuestionMapper;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.User;
+import life.majiang.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private QuestionService questionService;
+
+
+    @GetMapping("/")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
+        return "index";
+    }
+
+}
+
---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
+    public void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+
+    @Select("select * from user where id = #{id}")      //自动将从数据库找出的数据放入User对象中，但要注意驼峰规则
+    User findById(@Param("id") Long id);
+}
